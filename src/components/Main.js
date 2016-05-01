edaaa96926c4471f2c7ee2adb4d67b2a664108d8
A.T.M <1295213184@qq.com>
2016-05-01 14:34:41 +0800

    add img list to show img

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,25 +2,41 @@ require('normalize.css/normalize.css');
 require('styles/main.scss');
 
 import React from 'react';
+import data from '../sources/data.json';
 
-let yeomanImage = require('../images/yeoman.png');
-
-class AppComponent extends React.Component {
-  render() {
-    return (
-      <section className="stage">
-          <section className="imgs">
-
-          </section>
-          <nav className="nav">
-
-          </nav>
-      </section>
-    );
-  }
+class ImageFigure extends React.Component {
+    render() {
+        return (
+            <figure className="img-figure">
+                <img src={this.props.data.url}
+                     alt={this.props.data.title}/>
+                <figcaption>
+                    <h2 className="img-title">{this.props.data.title}</h2>
+                </figcaption>
+            </figure>
+        );
+    }
 }
 
-AppComponent.defaultProps = {
-};
+
+class AppComponent extends React.Component {
+    render() {
+        return (
+            <section className="stage">
+                <section className="imgs">
+                    {data.map(function (value) {
+                        console.log(value);
+                        return <ImageFigure key={value.name} data={value}/>;
+                    })}
+                </section>
+                <nav className="nav">
+
+                </nav>
+            </section>
+        );
+    }
+}
+
+AppComponent.defaultProps = {};
 
 export default AppComponent;
